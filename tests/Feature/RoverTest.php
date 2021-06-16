d8e24215abb53451d07b7df1cd17548849d9d259
Jim Taylor <jim@jimtaylor.space>
2021-06-16 20:38:46 +0100

    Basic tests passing for showing and creating

---
+++ tests/Feature/RoverTest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use App\Models\Rover;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class RoverTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function anyone_can_list_all_rovers()
+    {
+        $response = $this->get('/rovers');
+        $response->assertSuccessful();
+        $response->assertJsonPath('message', "Here's a list of all the rovers on Mars");
+        $response->assertJsonCount(Rover::count(), 'data');
+        $this->assertNotEmpty($response->json()['data'][Rover::inRandomOrder()->first()->id]['id']);
+    }
+
+    /**
+     * @test
+     */
+    public function anyone_can_show_a_rover()
+    {
+        $rover = Rover::inRandomOrder()->first();
+        $response = $this->get("/rovers/$rover->id");
+        $response->assertSuccessful();
+        $response->assertJsonPath('message', "Here's the rover you asked for");
+        $response->assertJsonCount(6, 'data');
+        $this->assertEquals($response->json()['data']['id'], $rover->id);
+        $this->assertEquals($response->json()['data']['x'], $rover->x);
+        $this->assertEquals($response->json()['data']['y'], $rover->y);
+        $this->assertEquals($response->json()['data']['direction'], $rover->direction);
+    }
+
+    /**
+     * @test
+     */
+    public function requesting_a_rover_that_doesnt_exist_returns_not_found()
+    {
+        $notARover = Rover::latest()->first()->id + 1;
+        $response = $this->get("/rovers/$notARover");
+        $response->assertNotFound();
+        $response->assertSeeText('404');
+        $response->assertSeeText('Not Found');
+    }
+
+    /**
+     * @test
+     */
+    public function anyone_can_create_a_rover()
+    {
+        $data = Rover::factory()->raw();
+        $response = $this->post('/rovers', $data);
+        $response->assertSuccessful();
+        $response->assertJsonPath('message', "Great news, you've just created a new Rover!");
+        $response->assertJsonCount(6, 'data');
+        $this->assertEquals($response->json()['data']['x'], $data['x']);
+        $this->assertEquals($response->json()['data']['y'], $data['y']);
+        $this->assertEquals($response->json()['data']['direction'], $data['direction']);
+    }
+}
