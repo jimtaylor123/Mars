000aa6830dcfa7e9b201b8ee7effc42294e83b7e
Jim Taylor <jim@jimtaylor.space>
2021-06-16 19:56:44 +0100

    Added index endpoint

--- app/Http/Controllers/RoverController.php
+++ app/Http/Controllers/RoverController.php
@@ -4,12 +4,15 @@ namespace App\Http\Controllers;
 
 use App\Models\Rover;
 use Illuminate\Http\Request;
+use App\Http\Resources\RoverCollection;
 
 class RoverController extends Controller
 {
     public function index()
     {
-        
+        return RoverCollection::make(Rover::all())->additional([
+            'message' => 'Here\'s a list of all the rovers on Mars',
+        ]);
     }
 
     public function store(Request $request)
