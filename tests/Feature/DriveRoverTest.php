8f377acec86169836d5bb883860f3464eccc2879
Jim Taylor <jim@jimtaylor.space>
2021-06-16 21:07:20 +0100

    Pencilled in some rover driving tests

---
+++ tests/Feature/DriveRoverTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class DriveRoverTest extends TestCase
+{
+   /**
+     * @test
+     */
+    public function a_rover_cannot_be_driven_with_an_invalid_command()
+    {
+      // TODO
+    }
+
+    /**
+     * @test
+     */
+    public function rover_commands_can_be_submitted_in_any_case()
+    {
+      // TODO
+    }
+
+    /**
+     * @test
+     */
+    public function a_rover_can_be_driven_correctly_with_simple_commands()
+    {
+      // TODO
+    }
+
+    /**
+     * @test
+     */
+    public function a_rover_can_be_driven_with_a_lowercase_command()
+    {
+      // TODO
+    }
+
+    /**
+     * @test
+     */
+    public function a_rover_can_be_driven_correctly_with_edge_case_commands()
+    {
+      // TODO
+    }
+
+
+}
